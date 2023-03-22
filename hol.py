f76b56ff8209bee902fceab6267bd013f9eb6c8c
Advittt <58274868+Advittt@users.noreply.github.com>
2023-03-22 15:38:38 +0000

    not needed - tester file

--- hol.py
+++
@@ -1,8 +0,0 @@
-        # For chnage of direction only
-        for i, car in enumerate(cars):
-            output = nets[i].activate(car.get_data())
-            choice = output.index(max(output))
-            if choice == 0:
-                car.angle += 10 # Left
-            else:
-                car.angle -= 10 # Right
